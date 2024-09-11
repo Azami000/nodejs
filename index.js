566082e20bb64e545513a73b547caa862d71e77c
Azami000 <chaka770@gmail.com>
2024-09-11 12:25:01 +0800

    sign up

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// const express = require("express");
-
-// const port = 8000;
-// const app = express();
-
-// app.use(express.json());
-
-// app.get("/", (request, response) => {
-//   response.send("Successfully intialized the express rest api");
-// });
-// app.post("/", (req, res) => {
-//   res.send("post request is successfully.");
-// });
-// app.put("/", (req, res) => {
-//   res.send("Put request is successfully.");
-// });
-// app.delete("/", (req, res) => {
-//   res.send("Delete request is successfully.");
-// });
-// app.listen(port, () => {
-//   console.log(`localhost${port}`);
-// });
-
 const express = require("express");
 const port = 8000;
 const app = express();
@@ -28,77 +5,46 @@ app.use(express.json());
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
-const DbModel = {
-  users: [
-    {
-      userId: String,
-      username: String,
-      profilePic: String,
-    },
-  ],
-  posts: [
-    {
-      postId: String,
-      userId: String,
-      title: String,
-      description: String,
-      image: String,
-      publishedAt: Date,
-    },
-  ],
-};
+app.post("/signup", (req, res) => {
+  const { username, email, password } = req.body;
 
-app.get("/users", (req, res) => {
-  const resultJson = fs.readFileSync("./db.json", "utf-8");
+  const resultJson = fs.readFileSync("./ss.json", "utf-8");
   const result = JSON.parse(resultJson);
-  const postId = uuidv4();
-  const publishedAt = new Date().toISOString();
 
-  const user = result.users.find((el) => el.userId === id);
-  if (!user) {
+  const user = result.users.find((el) => el.username === username);
+
+  if (user) {
     res.status(404);
-    res.send(`user not found following id = ${id}`);
+    res.send(`user bainaa`);
     return;
   }
 
-  res.send(user);
+  result.users.push({ username, email, password });
 
-  result.res.send(result);
+  fs.writeFileSync("./ss.json", JSON.stringify(result));
+
+  res.send(username);
 });
-app.get("posts/", async (req, res) => {
-  const resultJson = await fs.readFileSync("./db.json", "utf-8");
+
+app.post("/login", (req, res) => {
+  const { username, email, password } = req.body;
+  const resultJson = fs.readFileSync("./ss.json", "utf-8");
   const result = JSON.parse(resultJson);
+
+  const user = result.users.find((el) => el.username === username);
+
+  console.log(user);
+
+  if (!user || user.password !== password) {
+    res.status(404);
+    res.send(`ali neg n buruu bn`);
+  }
+
+  fs.writeFileSync("./ss.json", JSON.stringify(result));
+
+  res.send(username);
 });
 
-app.delete("/delete", (req, res) => {
-  // const { comments, posts, userId } = req.body;
-
-  const { postId } = req.body;
-  const resultJson = fs.readFileSync("./db.json", "utf-8");
-  const result = JSON.parse(resultJson);
-  const publishedAt = new Date().toISOString();
-
-  const deletePost = result.posts.filter((el) => el.postId !== postId);
-  result.posts = deletePost;
-
-  fs.writeFileSync("./db.json", JSON.stringify(result), "utf-8");
-  res.send("Succesfully deleted");
-});
-app.delete("/deletecomment", (req, res) => {
-  const { commentId } = req.body;
-  const resultJson = fs.readFileSync("./db.json", "utf-8");
-  const result = JSON.parse(resultJson);
-  const publishedAt = new Date().toISOString();
-
-  const deleteComment = result.comments.filter(
-    (el) => el.commentId !== commentId
-  );
-  result.comments = deleteComment;
-  fs.writeFileSync("./db.json", JSON.stringify(result), "utf-8");
-  console.log(deleteComment);
-
-  res.send("Succesfully comdeleted");
-});
 app.listen(port, () => {
   console.log(`localhost:${port}`);
 });
